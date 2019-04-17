f523ef5e9769f1075ce6599640781db76adac784
Elise Phan <aliceconnic@gmail.com>
2019-04-17 23:57:04 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -42,7 +42,7 @@ ReactDOM.render((
       <Route path="/" render={Home} />
       <Route exact path="/about" render={About} />
       <Route exact path="/login" render={Login} />
-      
+    </React.Fragment>    
   </Router>),
   document.getElementById('root')
 );
