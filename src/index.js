0c28ac4257c79c2093c5b61348617d75ce16e7a0
Elise Phan <aliceconnic@gmail.com>
2019-04-18 00:04:44 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -15,11 +15,11 @@ const Navbar = () =>
   <div>
     <NavLink
       to="/"
-      /* set exact so it knows to only set activeStyle when route is deeply equal to link */
+     
       exact
-      /* add styling to Navlink */
+ 
       style={link}
-      /* add prop for activeStyle */
+
       activeStyle={{
         background: 'darkblue'
       }}
