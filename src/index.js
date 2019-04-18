cdef4883a6f2ac3ea9659bd1ac0f99da620f654b
Elise Phan <aliceconnic@gmail.com>
2019-04-18 00:03:52 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -2,6 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
+const link = {
+  width: '100px',
+  padding: '12px',
+  margin: '0 6px 6px',
+  background: 'blue',
+  textDecoration: 'none',
+  color: 'white',
+}
+
+
 const Home = () => {
   return (
     <div>
